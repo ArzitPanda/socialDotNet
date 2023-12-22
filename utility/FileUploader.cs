b654a79b0364d9a7a9a9335c927df15d0bdf8777
arijit panda <arijit@spikewell.com>
2023-12-22 11:07:38 +0530

    feature:adding cloudinary sucessfully with transforming image quality to  low

--- utility/FileUploader.cs
+++ utility/FileUploader.cs
@@ -7,33 +7,52 @@ namespace sample_one.utility
 {
 
     public class FileUploader : IFileUploader
-    {   
-        public async  Task<string> UploadImage(IFormFile file)
+    {
+        public async Task<string> UploadImage(IFormFile file)
         {
+
 
             // Account account = new Account("dotnet_sample", "566762456861656", "vYNXOkanqyBMFdbAKAjYpcSduzw");
-            Account account = new Account ("dotnet_sample","566762456861656","vYNXOkanqyBMFdbAKAjYpcSduzw");
-              Cloudinary _cloudinary = new Cloudinary(account);
-            
+            Account account = new Account("djaj72xyk", "566762456861656", "vYNXOkanqyBMFdbAKAjYpcSduzw");
+            Cloudinary _cloudinary = new Cloudinary(account);
+            var uploadResult = new ImageUploadResult();
 
             if (file == null || file.Length == 0)
-        {
-            throw new  Exception("No file uploaded");
-        }
+            {
+                throw new Exception("No file uploaded");
+            }
 
-        // Upload and compress the image
-        var uploadParams = new ImageUploadParams
-        {
-            File = new FileDescription(file.FileName, file.OpenReadStream()),
-            // Transformation = new Transformation().Quality("auto:low").FetchFormat("auto")
-        };
+            using (var stream = file.OpenReadStream())
+            {
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Quality("auto:low").FetchFormat("auto")
+                };
 
-        var uploadResult = await _cloudinary.UploadAsync(uploadParams) ?? throw new Exception("Cloudinary upload failed");
-            Console.WriteLine("line no");
-            Console.WriteLine(uploadResult);
-        // Get the public URL of the uploaded image
-        var imageUrl = uploadResult.CreatedAt.ToString();
-        return imageUrl;
+                uploadResult = _cloudinary.Upload(uploadParams);
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+            // Upload and compress the image
+            // var uploadParams = new ImageUploadParams
+            // {
+            //     File = new FileDescription(file.FileName, file.OpenReadStream()),
+            //     // Transformation = new Transformation().Quality("auto:low").FetchFormat("auto")
+            // };
+            var imageUrl = uploadResult.Uri.ToString();
+            Console.WriteLine(imageUrl);
+            return imageUrl;
         }
     }
 
